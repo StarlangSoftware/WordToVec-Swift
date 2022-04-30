764e99032f21f76a645b69629afd2db0bc762cec
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-30 12:03:56 +0300

    Updated Corpus dependency.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["WordToVec"]),
     ],
     dependencies: [
-        .package(name: "Corpus", url: "https://github.com/StarlangSoftware/Corpus-Swift.git", .exact("1.0.16")),
+        .package(name: "Corpus", url: "https://github.com/StarlangSoftware/Corpus-Swift.git", .exact("1.0.17")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
